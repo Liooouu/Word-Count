c4a9435224f52525fad1bf7da4675cde958f7ade
Liooouu <apiadojohnliol@gmail.com>
2025-06-14 20:35:45 +0800

    added audio

--- Wcount.js
+++ Wcount.js
@@ -8,3 +8,13 @@ function resetFields() {
     document.getElementById("text-area").value = "";
     document.getElementById("nums").innerHTML= "";
 }
+
+const textArea = document.getElementById("text-area");
+const sound = document.getElementById("key-sound");
+
+textArea.addEventListener("keydown",() => {
+    sound.currentTime = 0;
+    sound.play();
+
+});
+
